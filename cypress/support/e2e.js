15da61d9306e30c64402d2ad8fad1adda82de240
Rogerio0Vieira <rogeriovbh@gmail.com>
2025-09-18 17:21:22 -0300

    fix: corrige problema de cors

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
 import './commands'
