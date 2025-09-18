c510f3ddfce7f72e1590fd02b626bef4a1d0d619
Rogerio0Vieira <rogeriovbh@gmail.com>
2025-09-18 15:31:43 -0300

    chore: altera arquivo de configuracao do cypress para modificar as pastas

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://demoqa.com",
-    specPattern: "cypress/e2e/**/*.feature",
-    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/features/**/*.feature",
+    supportFile: false,
+    stepDefinitions: "cypress/e2e/step_definitions/**/*.{js,ts}",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on(
